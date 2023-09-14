bfc784e01ada9670eb40fe677811ab50a3b227b4
KarolDiniz <dinizkaroline923@gmail.com>
2023-09-13 21:39:30 -0300

    :sparkles: Implementing bootswatch

--- animaladoptionfront/src/App.js
+++ animaladoptionfront/src/App.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import UserCreateForm from './components/UserCreateForm.js'; 
-
+import React, { useState } from 'react';
+import AdminLoginForm from './components/AdminLoginForm.js'; // Crie este componente para o formulário de login do administrador
+import Menu from './components/menu.js'
 function App() {
+  const [userToken, setUserToken] = useState(null);
+
+  // Função para definir o token do usuário após o login do administrador
+  const setAdminToken = (token) => {
+    setUserToken(token);
+  };
+
+  // Se o token for igual a 'roleadmin', renderize o menu
+  if (userToken === 'roleadmin') {
+    return (
+      <div className="App">
+        <Menu />
+      </div>
+    );
+  }
+
+  // Se não, renderize o formulário de login do administrador
   return (
     <div className="App">
-      {}
-      <UserCreateForm />
+      <AdminLoginForm setAdminToken={setAdminToken} />
     </div>
   );
 }
 
 export default App;
+
