10b28561c22b5b414c3458d1130dfc199d1b568a
KarolDiniz <dinizkaroline923@gmail.com>
2023-09-13 21:41:38 -0300

    :sparkles: Adding a new feature

---
+++ animaladoptionfront/src/components/UpdateAnimalForm.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+
+class UpdateAnimalForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '', // Allow updating name
+      species: '', // Allow updating species
+      description: '', // Allow updating description
+    };
+  }
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = () => {
+    const { name, species, description } = this.state;
+    const animalId = this.props.animalId; // Assume you pass the animal ID as a prop
+    const updatedAnimal = {
+      name,
+      species,
+      description,
+    };
+
+    fetch(`http://localhost:8080/api/animals/${animalId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedAnimal),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Animal updated:', data);
+      })
+      .catch((error) => {
+        console.error('Error updating animal:', error);
+      });
+  };
+
+  render() {
+    const { name, species, description } = this.state;
+
+    return (
+      <div>
+        <h2>Update Animal</h2>
+        <form>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={name}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="species">Species:</label>
+            <input
+              type="text"
+              id="species"
+              name="species"
+              value={species}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="description">Description:</label>
+            <textarea
+              id="description"
+              name="description"
+              value={description}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <button type="button" onClick={this.handleSubmit}>
+            Update Animal
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default UpdateAnimalForm;
---
+++ animaladoptionfront/src/components/UpdateUserForm.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+
+class UpdateUserForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userId: '', // Add a new state property for user ID
+      username: '', // Only allow updating username
+    };
+  }
+
+  handleInputChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = () => {
+    const { userId, username } = this.state;
+    const updateUser = {
+      username,
+    };
+
+    fetch(`http://localhost:8080/api/users/${userId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updateUser),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('User updated:', data);
+      })
+      .catch((error) => {
+        console.error('Error updating user:', error);
+      });
+  };
+
+  render() {
+    const { userId, username } = this.state;
+
+    return (
+      <div>
+        <h2>Update User</h2>
+        <form>
+          <div>
+            <label htmlFor="userId">User ID:</label>
+            <input
+              type="text"
+              id="userId"
+              name="userId"
+              value={userId}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="username">New Username:</label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={this.handleInputChange}
+            />
+          </div>
+          <button type="button" onClick={this.handleSubmit}>
+            Update User
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default UpdateUserForm;
